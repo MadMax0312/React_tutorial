b1f37636c504a4f67a9406bfc51481d29a189bc4
MadMax0312 <thahirmuhammedap@gmail.com>
2024-02-14 20:24:06 +0530

    props, propTypes and defaultProps

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './card.jsx'
 import Button from './Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
 
@@ -11,9 +12,16 @@ function App() {
         {/* <Header/>
         <Food/>
         <Footer/> 
-        <Card/>*/
+        <Card/>
+        <Button/>*/
         }
-        <Button/>
+        <Student name="Max" age={25} isStudent={true}/>
+        <Student name = "Sita" />
+        <Student name="Raju" age={15} isStudent={false}/>
+        <Student name="Vishnu" age={50} isStudent={true}/>
+        <Student name="Jayan" age={7} isStudent={false}/>
+        <Student/>
+        
         </>
     );
 }
