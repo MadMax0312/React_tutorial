62e020be5560d6d21fe98ff65dacbdd3fcc0769d
MadMax0312 <thahirmuhammedap@gmail.com>
2024-02-16 12:06:40 +0530

    Button Counter

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,12 @@
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 function App() {
     return (
         <>
-        <MyComponent/>
+        <Counter/>
         </>
     );
 }
 
 export default App
- 
+  
