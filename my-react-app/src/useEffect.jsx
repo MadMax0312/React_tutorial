339b4c8c1d943f4a0a6114e072046a6dcd56c068
MadMax0312 <thahirmuhammedap@gmail.com>
2024-02-29 10:55:42 +0530

    useEffect with dependencies

--- my-react-app/src/useEffect.jsx
+++ my-react-app/src/useEffect.jsx
@@ -3,18 +3,30 @@ import React, {useState, useEffect} from 'react'
 function MyComponent() {
 
     const [count, setCount] = useState(0)
+    const [color, setColor] = useState('blue')
 
     useEffect(()=>{
         document.title = `Count : ${count}`
-    },[])
+    },[count])
 
     function addCount() {
         setCount(c=> c + 1)
     }
+
+    function subtractCount() {
+        setCount(c => c - 1)
+    }
+
+    function changeColor() {
+        setColor(c => (c === 'red' ? 'orange' : 'red'));
+    }
+
   return (
-    <div>
-        <p>Count: {count}</p>
+    <div style={{background: color}}>
+        <p >Count: {count}</p>
         <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button>
+        <button onClick={changeColor}>Change Color</button>
     </div>
   )
 }
